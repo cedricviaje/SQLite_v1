c6c2832b689afc568f8e41e1eae0c7e87955734a
heisenbergx32 <heisenbergx64@users.noreply.github.com>
2018-12-09 20:19:59 +0800

    Added activity_difficulty.xml
    no transitions yet

--- app/src/main/java/com/example/lanceviaje/sqlite_v1/MainActivity.java
+++ app/src/main/java/com/example/lanceviaje/sqlite_v1/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.lanceviaje.sqlite_v1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtStart;
+    private TextView txtLeaderboards;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,23 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseHandler db = new DatabaseHandler(this);
 
-        // Inserting Shop/Rows
+/*        // Inserting Rows
         Log.d("Insert: ", "Inserting ..");
         db.addData(new Player(1, "Cedric", 100));
         db.addData(new Player(2, "Lance", 200));
         db.addData(new Player(3, "Jon", 300));
         db.addData(new Player(4, "Earon", 400));
 
-        // Reading all shops
+        // Reading all players in leaderboard
         Log.d("Reading: ", "Reading all players inside Leaderboards . . .");
         List<Player> players = db.getAllData();
 
         for (Player player: players) {
             String log = "ID: " + player.getId() + " , Name: " + player.getName() + " , Score: " + player.getScore();
-            // Writing shops  to log
+            // Writing players  to log
             Log.d("Player: ", log);
-        }
+        }*/
 
+        txtStart = findViewById(R.id.txtStart);
+        txtLeaderboards = findViewById(R.id.txtLeaderboards);
 
     }
+
+    private void setDifficulty(View v){
+
+        
+
+    }
+
 }
